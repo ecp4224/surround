c8975224ed8b1dd22c457c194501f90f251963d5
Eddie Penta <ecp4224@gmail.com>
2015-04-11 17:04:46 -0400

    Fixed validateDatabase function

--- backend/datastore.js
+++ backend/datastore.js
@@ -6,7 +6,7 @@ var database;
 var songCollection;
 var userCollection;
 
-var connectToDatabase = function () {
+var connectToDatabase = function (completed) {
     MongoClient.connect(url, function (err, db) {
         if (err)
             throw err;
@@ -17,16 +17,17 @@ var connectToDatabase = function () {
 
         songCollection = db.collection('songs');
         userCollection = db.collection('users');
+
+        completed();
     });
 };
 
-var validateDatabase = function() {
+var validateDatabase = function(completed) {
     if (!database) {
-        connectToDatabase();
+        connectToDatabase(completed);
     }
-
-    if (!database.serverConfig.isConnected())
-        connectToDatabase();
+    else if (!database.serverConfig.isConnected())
+        connectToDatabase(completed);
 };
 
 
@@ -38,21 +39,21 @@ module.exports = {
     occurred, then invoke the errorCallback with the error as a parameter
      */
     getSongs: function(lat, long, completedCallback, errorCallback) {
-        validateDatabase();
+        validateDatabase(function() {
+            var lowerLat = lat - radius;
+            var highLat = lat + radius;
+            var lowerLong = long - radius;
+            var highLong = long + radius;
 
-        var lowerLat = lat - radius;
-        var highLat = lat + radius;
-        var lowerLong = long - radius;
-        var highLong = long + radius;
-
-        songCollection.find({
-            'lat': {$gte: lowerLat, $lte: highLat},
-            'long': {$gte: lowerLong, $lte: highLong}
-        }).toArray(function(err, docs) {
-            if (err)
-                errorCallback(err);
-            else
-                completedCallback(docs);
+            songCollection.find({
+                'lat': {$gte: lowerLat, $lte: highLat},
+                'long': {$gte: lowerLong, $lte: highLong}
+            }).toArray(function(err, docs) {
+                if (err)
+                    errorCallback(err);
+                else
+                    completedCallback(docs);
+            });
         });
     },
 
@@ -62,54 +63,55 @@ module.exports = {
      occurred, then invoke the errorCallback with the error as a parameter
      */
     pushSong: function(lat, long, name, artist, completedCallback, errorCallback) {
-        validateDatabase();
+        validateDatabase(function() {
 
-        var lowerLat = lat - radius;
-        var highLat = lat + radius;
-        var lowerLong = long - radius;
-        var highLong = long + radius;
+            var lowerLat = lat - radius;
+            var highLat = lat + radius;
+            var lowerLong = long - radius;
+            var highLong = long + radius;
 
-        songCollection.find({
-            'name': name,
-            'artist': artist,
-            'lat': {$gte: lowerLat, $lte: highLat},
-            'long': {$gte: lowerLong, $lte: highLong}
-        }).toArray(function (err, docs) {
-            if (err) {
-                errorCallback(err);
-                return;
-            }
+            songCollection.find({
+                'name': name,
+                'artist': artist,
+                'lat': {$gte: lowerLat, $lte: highLat},
+                'long': {$gte: lowerLong, $lte: highLong}
+            }).toArray(function (err, docs) {
+                if (err) {
+                    errorCallback(err);
+                    return;
+                }
 
-            if (docs.length > 0) {
-                completedCallback(docs[0]);
-            } else {
-                songCollection.count({}, function (err, numOfDocs) {
-                    if (err) {
-                        errorCallback(err);
-                        return;
-                    }
-
-                    var song = {
-                        'id': numOfDocs,
-                        'latitude': lat,
-                        'longitude': long,
-                        'name': name,
-                        'artist': artist,
-                        'user_id': -1,
-                        'timePosted': Math.floor(new Date() / 1000),
-                        'genre': []
-                    };
-
-                    songCollection.insert(song, function (err, result) {
+                if (docs.length > 0) {
+                    completedCallback(docs[0]);
+                } else {
+                    songCollection.count({}, function (err, numOfDocs) {
                         if (err) {
                             errorCallback(err);
                             return;
                         }
 
-                        completedCallback(song);
+                        var song = {
+                            'id': numOfDocs,
+                            'latitude': lat,
+                            'longitude': long,
+                            'name': name,
+                            'artist': artist,
+                            'user_id': -1,
+                            'timePosted': Math.floor(new Date() / 1000),
+                            'genre': []
+                        };
+
+                        songCollection.insert(song, function (err, result) {
+                            if (err) {
+                                errorCallback(err);
+                                return;
+                            }
+
+                            completedCallback(song);
+                        });
                     });
-                });
-            }
+                }
+            });
         });
     }
 };
