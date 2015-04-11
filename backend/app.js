fea8daf52e420e96cbed46087c529a800298ed7d
Eddie Penta <ecp4224@gmail.com>
2015-04-11 16:24:23 -0400

    Finished first version of basic backend

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var songs = require('datastore.js');
+var app = express();
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+app.listen(8080);
+app.get('/api/fetch', function(req, res) {
+    if (!req.query.lat || !req.query.long) {
+        res.status(500);
+        res.send("Invalid request!");
+        return;
+    }
+
+    var lat = req.query.lat;
+    var long = req.query.long;
+
+    songs.getSongs(lat, long, function(found) {
+        res.send(JSON.stringify(
+            found
+        ));
+    }, function(e) {
+        res.status(500);
+        res.send(e);
+    });
+});
+
+app.post('/api/post', function(req, res) {
+    if (!req.body.lat || !req.body.long || req.body.songName || req.body.artist) {
+        res.status(500);
+        res.send("Invalid request!");
+        return;
+    }
+
+    var lat = req.body.lat;
+    var long = req.body.long;
+    var songName = req.body.songName;
+    var artist = req.body.artist;
+    songs.pushSong(lat, long, songName, artist, function(s) {
+        res.send(JSON.stringify(
+            s
+        ));
+    }, function(e) {
+        res.status(500);
+        res.send(e);
+    })
+});
---
+++ backend/datastore.js
@@ -0,0 +1,115 @@
+var radius = 0.00076346643;
+var MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/trails';
+var database;
+var songCollection;
+var userCollection;
+
+var connectToDatabase = function () {
+    MongoClient.connect(url, function (err, db) {
+        if (err)
+            throw err;
+
+        console.log("Connected to mongodb!");
+
+        database = db;
+
+        songCollection = db.collection('songs');
+        userCollection = db.collection('users');
+    });
+};
+
+var validateDatabase = function() {
+    if (!database) {
+        connectToDatabase();
+    }
+
+    if (!database.serverConfig.isConnected())
+        connectToDatabase();
+};
+
+
+module.exports = {
+
+    /*
+    Return an array of song objects.
+    When the function complete, invoke the completedCallback function with the array as a parameter, if an error
+    occurred, then invoke the errorCallback with the error as a parameter
+     */
+    getSongs: function(lat, long, completedCallback, errorCallback) {
+        validateDatabase();
+
+        var lowerLat = lat - radius;
+        var highLat = lat + radius;
+        var lowerLong = long - radius;
+        var highLong = long + radius;
+
+        songCollection.find({
+            'lat': {$gte: lowerLat, $lte: highLat},
+            'long': {$gte: lowerLong, $lte: highLong}
+        }).toArray(function(err, docs) {
+            if (err)
+                errorCallback(err);
+            else
+                completedCallback(docs);
+        });
+    },
+
+    /*
+     Return the song object saved/found
+     When the function complete, invoke the completedCallback function with the array as a parameter, if an error
+     occurred, then invoke the errorCallback with the error as a parameter
+     */
+    pushSong: function(lat, long, name, artist, completedCallback, errorCallback) {
+        validateDatabase();
+
+        var lowerLat = lat - radius;
+        var highLat = lat + radius;
+        var lowerLong = long - radius;
+        var highLong = long + radius;
+
+        songCollection.find({
+            'name': name,
+            'artist': artist,
+            'lat': {$gte: lowerLat, $lte: highLat},
+            'long': {$gte: lowerLong, $lte: highLong}
+        }).toArray(function (err, docs) {
+            if (err) {
+                errorCallback(err);
+                return;
+            }
+
+            if (docs.length > 0) {
+                completedCallback(docs[0]);
+            } else {
+                songCollection.count({}, function (err, numOfDocs) {
+                    if (err) {
+                        errorCallback(err);
+                        return;
+                    }
+
+                    var song = {
+                        'id': numOfDocs,
+                        'latitude': lat,
+                        'longitude': long,
+                        'name': name,
+                        'artist': artist,
+                        'user_id': -1,
+                        'timePosted': Math.floor(new Date() / 1000),
+                        'genre': []
+                    };
+
+                    songCollection.insert(song, function (err, result) {
+                        if (err) {
+                            errorCallback(err);
+                            return;
+                        }
+
+                        completedCallback(song);
+                    });
+                });
+            }
+        });
+    }
+};
