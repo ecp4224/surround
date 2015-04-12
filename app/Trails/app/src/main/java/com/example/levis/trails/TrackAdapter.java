f741a125a252f0a6e53624a11749669c693af878
Eddie Penta <ecp4224@gmail.com>
2015-04-12 10:37:57 -0400

    Making listview all fancy

--- app/Trails/app/src/main/java/com/example/levis/trails/TrackAdapter.java
+++ app/Trails/app/src/main/java/com/example/levis/trails/TrackAdapter.java
@@ -4,38 +4,18 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.levis.trails.core.Song;
-import java.util.ArrayList;
-import java.util.List;
+import com.nhaarman.listviewanimations.ArrayAdapter;
+import com.nhaarman.listviewanimations.util.Insertable;
 
-public class TrackAdapter extends BaseAdapter {
-    public List<Song> songlist; //Our global dynamic song list
+public class TrackAdapter extends ArrayAdapter<Song> implements Insertable<Song> {
     private LayoutInflater mInflater;
 
     public TrackAdapter(Context context) {
-        songlist = new ArrayList<Song>();
         mInflater = LayoutInflater.from(context);
-    }
-
-    @Override
-    public int getCount() {
-        return songlist.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-
-        return songlist.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-
-        return songlist.get(position).getId();
     }
 
     @Override
@@ -56,24 +36,12 @@ public class TrackAdapter extends BaseAdapter {
             holder = (ViewHolder)convertView.getTag();
         }
 
-        Song song = songlist.get(position);
+        Song song = super.getItem(position);
 
         holder.name.setText(song.getArtist() + " - " + song.getSongName());
         holder.number.setText("" + (position + 1));
 
         return convertView;
-    }
-
-    public void setSongs(List<Song> songs) {
-        songlist.addAll(songs);
-    }
-
-    public void addSong(Song song){
-        songlist.add(0,song);
-    }
-
-    public void removeSongAt(int position){
-        songlist.remove(position);
     }
 
     static class ViewHolder {
